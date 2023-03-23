7e1d0eee37cd5978f4a5ea9f013b4c90303cac3b
Kevin Deal <kevin@superfriend.ly>
2023-03-22 21:58:52 -0400

    footer and cta

--- template-parts/blocks/footer-cta.php
+++ template-parts/blocks/footer-cta.php
@@ -13,6 +13,9 @@
     $link_url = '';
   }
 
+  $image = get_field('background_image');
+  $background_style = "background: url(" . $image .') center no-repeat; background-size: cover;';
+
   $url = home_url( $path = '/', $scheme = 'https' );
 ?>
 <?php if(get_field('link') ) : ?>
@@ -22,18 +25,16 @@
     $link_title = $link['title'];
   ?>
   <?php if(is_array($link) ) : ?>
-    <div class="footer-cta full-bleed cta-gradient flex relative overflow-visible max-h-auto <?php echo esc_attr($className); ?> mt-16">
-      <div class="relative bottom-0">
-        <div class="wp-block-gb-lottiefiles absolute -left-24 bottom-0 max-w-[100vw]" style="background-color:transparent"><lottie-player id="xnsuwzmn" interactivitytype="seek" interactivitymode="scroll" visibilitystart="0" visibilityend="1" framesstart="0" framesend="90" rest="{&quot;autoplay&quot;:false,&quot;loop&quot;:null,&quot;controls&quot;:null}" totalframes="90" mode="bounce" src="<?php echo $url; ?>wp-content/uploads/2022/11/data.json" speed="4" background="transparent" class="lottie-player mx-auto" style="width:650px;height:300px"></lottie-player></div>
-      </div>
-      <div class="mx-auto my-auto max-w-2xl px-4 sm:px-6 h-60 relative flex flex-col justify-center ml-[50%]">
-        <h2 class="text-4xl font-medium text-white sm:text-4xl">
-          <span class="block"><?php echo get_field('header'); ?></span>
-        </h2>
-        <?php if(!empty($link) ) : ?>
-          <a href="<?php echo esc_url($link_url)?>" class="mt-8 w-full justify-center text-base font-medium text-turquoise sm:w-auto"><?php echo esc_html($link_title) ?><svg class="inline ml-2" height="16" width="16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"></path></svg></a> 
-        <?php endif ?>
-
+    <div class="max-h-auto <?php echo esc_attr($className); ?>" style="<?php echo $background_style; ?>">
+      <div class="w-full footer-cta">
+        <div class="mx-auto my-auto max-w-2xl px-4 sm:px-6 h-screen  relative flex flex-col justify-center">
+          <h2 class="font-medium text-white pb-20 text-center text-4xl">
+            <span class="block"><?php echo get_field('header'); ?></span>
+          </h2>
+          <?php if(!empty($link) ) : ?>
+            <a href="<?php echo esc_url($link_url)?>" class="bg-yellow mx-auto rounded-full border border-transparent px-12 py-2 text-base font-medium text-black shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"><?php echo esc_html($link_title) ?></a> 
+          <?php endif ?>
+        </div>
       </div>
     </div>
   <?php endif ?>
